062cb96d21b36bd0781cc002292f2b7460c068a4
AubreyVel <agvelasco@amarillocollege.com>
2025-03-17 16:57:49 -0500

    Third code change

--- script.js
+++ script.js
@@ -2,5 +2,13 @@
 window.alert("Good afternoon.");
 //declare the variable to hold the div element
 let container = document.getElementById("container");
+//getElementByID i sth emethod to locate the element by th eID
+//container is an HTML lement, it is a div in this case
 //set th econtent of element
+
 container.innerHTML = "This is the container place";
+//change the color style into red color
+container.style.color = "red";
+
+//add new element p into yhe page
+document.write("<p>This is a new paragraph</p>");
