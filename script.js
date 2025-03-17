2bec000cbf6dcdbd835e8d52d24384211bb00d81
AubreyVel <agvelasco@amarillocollege.com>
2025-03-17 16:42:43 -0500

    commit 2 i think

---
+++ script.js
@@ -0,0 +1,6 @@
+//display alert goof morning
+window.alert("Good afternoon.");
+//declare the variable to hold the div element
+let container = document.getElementById("container");
+//set th econtent of element
+container.innerHTML = "This is the container place";
